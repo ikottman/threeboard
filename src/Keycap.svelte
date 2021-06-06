59aa105f90a8714ff1a257a194bb2d6afb6be78a
Ian Kottman <ikottman@users.noreply.github.com>
2021-06-06 18:17:38 -0500

    extract legend component

--- src/Keycap.svelte
+++ src/Keycap.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
-  import Sphere from './Sphere.svelte';
   import {
     ExtrudeGeometry,
     Shape,
     Mesh,
     MeshStandardMaterial,
-    TextGeometry,
-    FontLoader,
   } from "svelthree";
-  import { toRadians } from './utils';
-  import { onMount } from 'svelte';
+  import Legend from './Legend.svelte';
 
   export let scene;
   // dimensions
   export let unit; // keycap base unit in mm.
   export let ratio = 1; // ratio compared to the unit. a letter is 1u
   export let margin;
-  export let degrees; // angle of the right slope in the side profile
   export let legend; // text to render on top
   let width, height, depth;
   width = height = depth = (unit * ratio - margin);
@@ -34,7 +29,7 @@
    *        (x,y)/__|_____|___\ (x4,y)
    *                   w
    */
-  function getProfile(x: number, y:number, w: number, l: number, t: number, r: number, s: number) {
+   function getProfile(x: number, y:number, w: number, l: number, t: number, r: number, s: number) {
     // special thanks to Haley for the maths
     const x2 = Math.sqrt((l*l) - (t*t));
     const x3 = w - Math.sqrt((r*r) - (s*s));
@@ -63,22 +58,6 @@
   $: profile = getProfile(x, y, width, 11, 10, 8, 7);
   $: geometry = make3d(profile, depth);
   let material = new MeshStandardMaterial();
-
-  // load font
-  const loader = new FontLoader();
-  let text;
-  const fontSize = 4;
-  onMount(async () => {
-    loader.load( 'fonts/Roboto_Regular.json', (loadedFont) => {
-      text = new TextGeometry(legend, {
-        font: loadedFont,
-        size: 4,
-        height: 0.1,
-      });
-      text.rotateX(toRadians(-90));
-      text.rotateY(toRadians(90));
-    });
-  });
 </script>
 
 <Mesh
@@ -91,13 +70,4 @@
   castShadow
 />
 
-{#if text}
-  <Mesh
-    {scene}
-    geometry={text}
-    material={new MeshStandardMaterial()}
-    mat={{ color: 0x000000 }}
-    pos={[x + (width / 2) - fontSize, y + height + 0.005, z + depth / 2]}
-    scale={[1, 1, 1]}
-  />
-{/if}
+<Legend legend={legend} x={x} y={y} z={z} width={width} height={height} depth={depth} scene={scene} />
---
+++ src/Legend.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import {
+    Mesh,
+    MeshStandardMaterial,
+    TextGeometry,
+    FontLoader,
+  } from "svelthree";
+  import { toRadians } from './utils';
+  import { onMount } from 'svelte';
+
+  export let scene;
+  export let legend; // text to render on top
+  // position
+  export let x;
+  export let y;
+  export let z;
+  // keycap dimensions
+  export let width;
+  export let height;
+  export let depth;
+  const fontSize = 4;
+
+  // load font
+  const loader = new FontLoader();
+  let text;
+  onMount(async () => {
+    loader.load( 'fonts/Roboto_Regular.json', (loadedFont) => {
+      text = new TextGeometry(legend, {
+        font: loadedFont,
+        size: 4,
+        height: 0.1,
+      });
+      text.rotateX(toRadians(-90));
+      text.rotateY(toRadians(90));
+    });
+  });
+</script>
+
+{#if text}
+  <Mesh
+    {scene}
+    geometry={text}
+    material={new MeshStandardMaterial()}
+    mat={{ color: 0x000000 }}
+    pos={[x + (width / 2) - fontSize, y + height + 0.005, z + depth / 2]}
+    scale={[1, 1, 1]}
+  />
+{/if}
